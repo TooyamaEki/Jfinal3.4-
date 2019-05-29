e8a6dbb77ce95afde4d6eb31e6f904ec9905b4e2
zhenye <451949487@qq.com>
2019-05-29 20:58:26 +0800

    添加校验器

--- src/main/java/com/kakuhanashi/vaildator/user/LoginVaildator.java
+++ src/main/java/com/kakuhanashi/vaildator/user/LoginVaildator.java
@@ -12,6 +12,8 @@ public class LoginVaildator extends ValidatorExt{
 		
 //		String ExtAccount =  "^1([38][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\\d{8}$";
 //		validateRegex("account", ExtAccount, "error", "请输入手机号码");
+		
+		
 		System.out.println("校验器");
 		
 		
